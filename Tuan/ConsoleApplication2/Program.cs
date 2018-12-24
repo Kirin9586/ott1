429fd92b80d877311f23fbd21a5152b259f9b2a7
Kirin9586 <tuanho02k11@viendong.edu.vn>
2018-12-24 16:08:59 +0700

    nh

---
+++ Tuan/ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tuoi, namsinh = 1998;
+            String ten = "Ho Anh Tuan";
+            Console.Write("Hello nhoc {0}\n", ten);
+            tuoi = 2018 - namsinh;
+            Console.WriteLine("Tuoi {0}", tuoi);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
